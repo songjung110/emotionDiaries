b04b13a82ddb1b4bfef8fc3190e04117bb6f26b4
tjsjt <tjsjttt@gmail.com>
2026-02-14 14:00:20 +0900

    feat: 페이지별 라우터 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import { Navigate, Route, Routes } from "react-router-dom"
+import StartPage from "./pages/StartPage"
+import DiaryPage from "./pages/DiaryPage"
+import ChatPage from "./pages/ChatPage"
+import MainPage from "./pages/MainPage"
 
 
 function App() {
 
   return (
     <Routes>
-      <Route path="" element={<div>Home</div>} />
-      <Route path="/diary" element={<div>Diary</div>} />
-      <Route path="/chat" element={<div>Chat</div>} />
+      <Route path="/" element={<MainPage />} />
+      <Route path="/start" element={<StartPage/ >} />
+      <Route path="/diary" element={<DiaryPage/ >} />
+      <Route path="/chat" element={<ChatPage/ >} />
       <Route path="*" element={<Navigate to={'/'} />} />
     </Routes>
   )
