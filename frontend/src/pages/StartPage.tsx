a408c84af45399c765a9f6e2c3cb64faee7a1a8e
tjsjt <tjsjttt@gmail.com>
2026-02-16 15:10:56 +0900

    feat: 스타트 페이지에 로그인 기초로직 추가, api 통신 & cookies 확인 후 로그인 여부 확인 로직 추가

--- frontend/src/pages/StartPage.tsx
+++ frontend/src/pages/StartPage.tsx
@@ -1,6 +1,60 @@
+import { useState, type FormEvent } from 'react';
+import styles from './StartPage.module.css'
+import { checkUsername } from '../api/userApi';
+import { useNavigate } from 'react-router-dom';
+import useUser from '../hooks/useUser';
+
 function StartPage() {
-    return (
-        <div>Start Page</div>
-    )
+  const [usernameInput, setUsernameInput] = useState('')
+  const navigate = useNavigate();
+  const { setUsername } = useUser();
+  
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // 1. validation 검증.
+    if(!usernameInput.trim()) {
+      alert('이름을 입력해주세요.');
+      return;
+    }
+ 
+    // 2 API 통신
+    try {
+      const { status: duplicated } = await checkUsername(usernameInput);
+  
+      if(duplicated) {
+        alert("이미 해당 이름은 사용 중 입니다.");
+        return;
+      }
+  
+      // TODO: 로컬 쿠키에 username 저장.
+      setUsername(usernameInput);
+      navigate('/');
+
+    } catch (error) {
+      console.error(error);
+      alert('서버와의 통신 중 오류가 발생 했습니다.');
+    }
+  }
+  
+  return (
+    
+    <div className={styles.root}>
+      <div className={styles.content}>
+        <div className={styles.innerContent}>
+          <div className={styles.title}>일기를 통해 <br />내 감정을 알아보세요. </div>
+          <form onSubmit={handleSubmit} className={styles.form}>
+            <input 
+            value={usernameInput}
+            onChange={event => setUsernameInput(event.target.value)}
+            placeholder={'이름을 입력해주세요.'}
+            className={styles.input}
+            />
+            <button type={'submit'} className={styles.button}>시작</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
 }
 export default StartPage;
