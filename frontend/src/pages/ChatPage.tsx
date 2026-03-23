30064eaccc8542fe13b5636fb0abd506697bf43d
tjsjt <tjsjttt@gmail.com>
2026-03-23 21:43:44 +0900

    feat: 챗페이지 더미데이터 -> api 호출데이터로 수정, AI와의 채팅 기능 추가

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -1,41 +1,145 @@
 import { Fragment } from "react/jsx-runtime";
 import styles from "./ChatPage.module.css"
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import type { MessageDto } from "../type/message";
 import useUser from "../hooks/useUser";
+import { useEffect, useRef, useState, type FormEvent } from "react";
+import { getDiaryMessages, streamDiaryMessage } from "../api/messageApi";
 
-const DUMMY_MESSAGES: MessageDto[] = [
-    {
-        id: 0,
-        diaryId: 0,
-        type: 'MSG',
-        username: "HI",
-        message: "안녕~~"
-    },
-    {
-        id: 1,
-        diaryId: 1,
-        type: 'MSG',
-        username: "HI",
-        message: "안녕~~"
-    },
-    {
-        id: 2,
-        diaryId: 0,
-        type: 'MSG',
-        username: "JUNG",
-        message: "안녕~~"
-    }
-]
+
+function toImageSrc(base64: string) {
+    return `data:image/png;base64,${base64}`
+}
 
 function ChatPage() {
     const navigate = useNavigate();
     const { username } = useUser();
+    // navigate로 받은 값을 Location을 통해 받음
+    const location = useLocation();
+    const diaryId = location.state?.diaryId as number;
+    const initialImage = location.state?.initialImage as String;
 
-    const handleSubmit = () =>{
+    const [messages, setMessages] = useState<MessageDto[]>([])
+    const [inputText, setInputText] = useState('');
 
+    const scrollRef = useRef<HTMLDivElement>(null);
+
+    useEffect(()=>{
+        if(!diaryId || !username) {
+            navigate('/', {replace: true});
+            return;
+        }
+
+        const initialize = async () => {
+            console.log("init")
+            const response = await getDiaryMessages(diaryId);
+
+            if(initialImage && !response.find(message => message.type === "IMG")) {
+                const userImageMessage: MessageDto = {
+                    id: Date.now(),
+                    diaryId,
+                    type: 'IMG',
+                    username,
+                    message: initialImage,
+                }
+                const aiMessageId = Date.now() + 1;
+                const aiMessage: MessageDto = {
+                    id: aiMessageId,
+                    diaryId,
+                    type: 'MSG',
+                    username: "AI",
+                    message: "..."
+                }
+
+                setMessages([...messages, userImageMessage, aiMessage]);
+
+                let text = '';
+
+                await streamDiaryMessage({
+                    diaryId,
+                    type: "IMG",
+                    username,
+                    message: initialImage
+                }, (chunk) => {
+                    text += chunk;
+                    setMessages(prev => {
+                        const next = [...prev];
+                        const targetIndex = next.findIndex(m => m.id === aiMessageId);
+
+                        if(targetIndex !== -1) {
+                            next[targetIndex] = {
+                                ...next[targetIndex],
+                                message: text,
+                            };
+                        }
+
+                        return next;
+                    })
+                })
+            } else {
+                setMessages(response);
+            }
+        }
+        initialize();
+    }, [username, navigate, diaryId, initialImage])
+
+    useEffect(() => {
+        const scroll = scrollRef.current;
+        if(!scroll) return;
+        scroll.scrollTop = scroll.scrollHeight;
+    }, [messages])
+
+    const handleSubmit = async (event: FormEvent) =>{
+        event.preventDefault();
+        if(!inputText.trim() || !username) return;
+
+        setInputText('');
+
+        const userMessage: MessageDto = {
+            id: Date.now(),
+            diaryId,
+            type: "MSG",
+            username,
+            message: inputText
+        }
+
+        const aiMessageId = Date.now() + 1;
+        const aiMessage: MessageDto = {
+            id: aiMessageId,
+            diaryId,
+            type: "MSG",
+            username: "AI",
+            message: "..."
+        }
+
+        setMessages(prev => [...prev, userMessage, aiMessage]);
+
+        let text = '';
+        await streamDiaryMessage({
+            diaryId,
+            type: "MSG",
+            username,
+            message: inputText
+        }, chunk => {
+            text += chunk;
+            setMessages(prev => {
+                const next = [...prev];
+                const targetIndex = next.findIndex(m => m.id === aiMessageId);
+
+                if(targetIndex !== -1) {
+                    next[targetIndex] = {
+                        ...next[targetIndex],
+                        message: text,
+                    };
+                }
+
+                return next;
+            })
+        })
     };
+
+
     return (
         <>
             <header className={styles.header}>
@@ -48,8 +152,8 @@ function ChatPage() {
                     {'<'}
                 </Button>
             </header>
-            <div className={styles.chatList}>
-                {DUMMY_MESSAGES.map((message, index) => {
+            <div className={styles.chatList} ref={scrollRef}>
+                {messages.map((message, index) => {
                     const isMe = username === message.username;
                     return (
                         <div
@@ -62,7 +166,10 @@ function ChatPage() {
                                 {message.type == "MSG" ? (
                                     message.message
                                 ) : (
-                                    <img />
+                                    <img 
+                                        src={toImageSrc(message.message)}
+                                        className={styles.image}
+                                    />
                                 )}
                             </div>
                         </div>
@@ -73,6 +180,8 @@ function ChatPage() {
             <form className={styles.inputArea} onSubmit={handleSubmit}>
                 <input 
                     className={styles.input}
+                    value={inputText}
+                    onChange={event => setInputText(event.target.value)}
                 />
                 <Button
                     type={'submit'}
